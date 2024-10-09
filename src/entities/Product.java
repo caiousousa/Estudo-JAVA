1d78e555b84c67f6fe9f150c4db3ae0a43a6f7a8
caiousousa <caioetec2019@gmail.com>
2024-10-09 15:03:21 -0300

    testando

---
+++ src/entities/Product.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Product {
+    private String name;
+    private Double price;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public Product(Double price) {
+        this.price = price;
+    }
+}
+
+
